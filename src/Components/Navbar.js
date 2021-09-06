ede92fd367354be218b126d6645902a1ea3d7669
tanmaythole <tanmaythole@gmail.com>
2021-09-06 23:38:55 +0530

    Navbar elements HighLighted Using useLocation

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {Link} from "react-router-dom"
+import {Link, useLocation} from "react-router-dom"
 
 
 function Navbar({title, toggle, btnText}) {
-    
+    let location = useLocation();
 
     return (
         <>
@@ -17,10 +17,10 @@ function Navbar({title, toggle, btnText}) {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                        <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                        <Link className={`nav-link ${location.pathname==='/'?"active":""}`} aria-current="page" to="/">Home</Link>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/about">About</Link>
+                        <Link className={`nav-link ${location.pathname==='/about'?"active":""}`} to="/about">About</Link>
                     </li>
                     </ul>
                     <div className="form-check form-switch">
